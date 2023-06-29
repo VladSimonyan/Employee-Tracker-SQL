6c6caf6c75f6a851d0abf904d237600324de90a4
VladSimonyan <129136870+VladSimonyan@users.noreply.github.com>
2023-06-28 19:11:40 -0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,1 +1,17 @@
+const mysql = require("mysql");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3000,
+    user: "root",
+    password: "M0byNick",
+    database: "employeesDB",
+});
+
+connection.connect(function(err) {
+    if(err){
+        return Promise.reject(err);
+    }
+});
+
+module.exports = connection;
