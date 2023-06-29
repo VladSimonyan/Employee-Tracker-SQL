be1ba184ad91d243f9596f9906d0e0251337652e
VladSimonyan <129136870+VladSimonyan@users.noreply.github.com>
2023-06-28 19:12:07 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,27 @@
+DROP DATABASE IF EXISTS employeesDB;
 
+CREATE DATABASE employeesDB;
+USE employeesDB;
+
+CREATE TABLE employee (
+    employeeID INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    roleID INT,
+    managerID INT NULL,
+    CONSTRAINT foreignKEYROLE FOREIGN KEY (roleID) REFERENCES role(roelID) ON DELETE CASCADE;
+    CONSTRAINT foreignKEYMANAGER FOREIGN KEY (managerID) REFERENCES employee(employeeID) ON DELETE SET NULL
+);
+
+CREATE TABLE department (
+    deptID INT AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(40) NULL
+);
+
+CREATE TABLE role (
+    roleID INT AUTO_INCREMENT PRIMARY KEY,
+    roleTitle VARCHAR(40) NULL,
+    roleSalary DECIMAL NULL,
+    deptID INT NULL,
+    CONSTRAINT foreignKeyDepartment FOREIGN KEY (deptID) REFERENCES department(deptID) ON DELETE CASCADE
+);
